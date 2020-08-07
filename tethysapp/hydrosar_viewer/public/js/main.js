db9a8a545dfe0eda88aacda1360d4d3eb2108425
chrisedwards <chris3edwards3@gmail.com>
2020-08-07 09:13:16 -0600

    Minor JS edits

--- tethysapp/hydrosar_viewer/public/js/main.js
+++ tethysapp/hydrosar_viewer/public/js/main.js
@@ -4,14 +4,21 @@ Cookies.set('instance_id', instance_id);
 function csrfSafeMethod(method){return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));}
 $.ajaxSetup({beforeSend: function (xhr, settings) {if (!csrfSafeMethod(settings.type) && !this.crossDomain) {xhr.setRequestHeader("X-CSRFToken", csrftoken);}}});
 
+////////////////////////////////////////////////////////////////////////  Initialize the Map
 const map_obj = map();
-
 const my_basemaps = basemaps();
 map_obj.on("mousemove", function (event) {$("#mouse-position").html('Lat: ' + event.latlng.lat.toFixed(5) + ', Lon: ' + event.latlng.lng.toFixed(5));});
 let wms_obj = newWMS();
 legend.addTo(map_obj);
 controlsObj = makeControls();
 latlon.addTo(map_obj);
+
+////////////////////////////////////////////////////////////////////////  EVENT LISTENERS
+function update() {
+    layerWMS = newWMS();
+    controlsObj = makeControls();
+    legend.addTo(mapObj);
+}
 
 function requestTimeSeries(){
     let data = {};
